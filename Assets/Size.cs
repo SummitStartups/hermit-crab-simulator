a6be6c36bdf96474aef54913f11cab02762db847
Kenny Hwu <kennywhwu@gmail.com>
2019-01-26 11:06:45 -0800

    added restart and disable input when dead

---
+++ Assets/Size.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Size : MonoBehaviour {
+
+    float size;
+    float growTime = 5f;
+
+	// Use this for initialization
+	void Start () {
+        size = 1;
+        InvokeRepeating("Grow", growTime,growTime);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+    }
+
+    // Recurring growth
+    void Grow()
+    {
+        print("GROW");
+        print(size);
+        size++;
+    }
+}
