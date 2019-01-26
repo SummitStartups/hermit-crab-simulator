cc2a7eb376ed2cdf87b5bf905263d26e92c5e692
summitdavid <dg@summitstartups.com>
2019-01-26 11:52:59 -0800

    anther shellless fix

--- Assets/Scripts/Eating.cs
+++ Assets/Scripts/Eating.cs
@@ -6,8 +6,8 @@ public class Eating : MonoBehaviour
 {
 
     private Movement movement;
-	private float growthRate = 0.1f;
-	private float maxGrowthRate = 0.1f;
+    private float growthRate = 0.1f;
+    private float maxGrowthRate = 0.1f;
     void Start()
     {
         movement = GetComponent<Movement>();
@@ -18,9 +18,12 @@ public class Eating : MonoBehaviour
         {
             Destroy(col.gameObject);
             float scale = Mathf.Min(growthRate * transform.localScale.x, maxGrowthRate);
-            movement.shell.localScale *= transform.localScale.x / (scale + transform.localScale.x);
+            if (movement.shell != null)
+            {
+                movement.shell.localScale *= transform.localScale.x / (scale + transform.localScale.x);
+            }
             transform.localScale += Vector3.one * scale;
-			movement.speed += Mathf.Min(growthRate * scale);
+            movement.speed += Mathf.Min(growthRate * scale);
         }
     }
 }
