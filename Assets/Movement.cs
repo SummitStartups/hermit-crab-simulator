9eb0b96021e89044557b46e11021cd622a8d1513
Victor Marks <vjmarks@gmail.com>
2019-01-25 23:27:31 -0800

    hiding, charge, ExitShell

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,6 +6,9 @@ public class Movement : MonoBehaviour {
 
 	public Transform cameraObject;
 	public float speed = 2;
+	public bool hiding = false; // when true, character can't move
+	public bool charge = false;
+	public bool ExitShell = false; // when true, character is vulnerable from all sides
 
 	// Use this for initialization
 	void Start () {
@@ -15,17 +18,35 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 primaryTouchpad = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
-		if (primaryTouchpad.y > 0.2f || Input.GetKey(KeyCode.W) ) {
+		if (hiding == false) {
+			if (primaryTouchpad.y > 0.2f || Input.GetKey(KeyCode.W) ) {
+				transform.position += cameraObject.forward * speed / 100;
+			}
+			if (primaryTouchpad.y < -0.2f || Input.GetKey(KeyCode.S) ) {
+				transform.position -= cameraObject.forward * speed / 100;
+			}
+			if (primaryTouchpad.x > 0.2f || Input.GetKey(KeyCode.D) ) {
+				transform.position += cameraObject.right * speed / 100;
+			}
+			if (primaryTouchpad.x < -0.2f || Input.GetKey(KeyCode.A) ) {
+				transform.position -= cameraObject.right * speed / 100;
+			}
+		}
+
+		hiding = (OVRInput.Get(OVRInput.Button.PrimaryTouchpad) && primaryTouchpad.y < -0.2f) || Input.GetKey(KeyCode.H); 
+		charge = (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger)) || Input.GetKey(KeyCode.C );
+		ExitShell = (OVRInput.Get(OVRInput.RawButton.Back) || Input.GetKey(KeyCode.E));
+
+		if (hiding) {
+			// shell moves down covering player's vision
+		}
+
+		if (charge) {
 			transform.position += cameraObject.forward * speed / 100;
 		}
-		if (primaryTouchpad.y < -0.2f || Input.GetKey(KeyCode.S) ) {
-			transform.position -= cameraObject.forward * speed / 100;
-		}
-		if (primaryTouchpad.x > 0.2f || Input.GetKey(KeyCode.D) ) {
-			transform.position += cameraObject.right * speed / 100;
-		}
-		if (primaryTouchpad.x < -0.2f || Input.GetKey(KeyCode.A) ) {
-			transform.position -= cameraObject.right * speed / 100;
+
+		if (ExitShell) {
+			// shell moves up and becomes detached from player
 		}
 	}
 }
